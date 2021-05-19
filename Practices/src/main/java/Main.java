57059b44a631c7d183d6c0aecd7601e85b435d26
Daniel Sandoval Penaloza <Daniel.Sandoval@jalasoft.com>
2021-05-18 21:11:03 -0400

    Add variable type

--- Practices/src/main/java/Main.java
+++ Practices/src/main/java/Main.java
@@ -11,19 +11,19 @@ public class Main {
         list1.addFirst("e");
         list1.print();
 
-        MyCustomList list2 = new MyCustomList();
+        MyCustomList<Integer> list2 = new MyCustomList<>();
         list2.add(1);
         list2.addFirst(2);
         list2.addLast(0);
         list2.print();
 
-        MyCustomList list3 = new MyCustomList();
+        MyCustomList<Boolean> list3 = new MyCustomList<>();
         list3.add(true);
         list3.addFirst(false);
         list3.addLast(false);
         list3.print();
 
-        MyCustomList list4 = new MyCustomList();
+        MyCustomList<String> list4 = new MyCustomList<>();
         list4.add("x");
         list4.addFirst("y");
         list4.addLast("z");
